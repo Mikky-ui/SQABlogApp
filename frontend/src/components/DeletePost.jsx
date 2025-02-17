d1ef197a12e892877e4c61217b5bb4128f666b43
Mikky-ui <60062143+Mikky-ui@users.noreply.github.com>
2025-02-17 17:21:49 +0000

    Component temp pages

---
+++ frontend/src/components/DeletePost.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DeletePost = () => {
+    return(
+        <div>
+            This is the delete post
+        </div>
+    )
+}
+export default DeletePost
---
+++ frontend/src/components/EditPost.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EditPost = () => {
+    return(
+        <div>
+            This is the EditPost
+        </div>
+    )
+}
+export default EditPost
