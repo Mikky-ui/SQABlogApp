ea6c3b6923c81771f827cca0850e2c59b3d83471
Mikky-ui <60062143+Mikky-ui@users.noreply.github.com>
2025-02-17 17:30:14 +0000

    parameter path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
       <Routes>
         <Route path="" element={<Home/>}/>
         <Route path="/create" element={<CreatePost/>}/>
-        <Route path="/edit" element={<EditPost/>}/>
-        <Route path="/delete" element={<DeletePost/>}/>
+        <Route path="/edit/:id" element={<EditPost/>}/>
+        <Route path="/delete/:id" element={<DeletePost/>}/>
       </Routes>
     </>
   )
